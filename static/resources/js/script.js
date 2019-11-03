e889b7623389d8ec54049b8688d2b99bf27721e6
Damien <pond@live.co.za>
2019-11-03 20:07:56 +0200

    update

---
+++ static/resources/js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+    //Title fade out on scroll
+    $(".js--wp-1").waypoint(function(direction){
+        if(direction == 'down'){
+            $(".header-row").addClass("animated fadeOut");
+        }
+        else {
+            $(".header-row").removeClass("animated fadeOut");
+        }
+    },
+    {
+        offset: '70%;'
+    });
+
+    //Cards fade in
+    $(".js--wp-1").waypoint(function(direction){
+        if(direction == 'down'){
+            $(".first-fade").addClass("animated fadeInUp");
+        }
+    },
+    {
+        offset: '55%;'
+    });
+
+    $(".js--wp-1").waypoint(function(direction){
+        if(direction == 'down'){
+            $(".second-fade").addClass("animated fadeInUp");
+        }
+    },
+    {
+        offset: '50%;'
+    });
+
+    $(".js--wp-1").waypoint(function(direction){
+        if(direction == 'down'){
+            $(".third-fade").addClass("animated fadeInUp");
+        }
+    },
+    {
+        offset: '45%;'
+    });
+
+    $(".js--wp-1").waypoint(function(direction){
+        if(direction == 'down'){
+            $(".fourth-fade").addClass("animated fadeInUp");
+        }
+    },
+    {
+        offset: '35%;'
+    });
+});
